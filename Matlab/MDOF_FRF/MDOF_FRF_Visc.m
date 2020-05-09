4bc29f9eba946beafd7b04fb1c1c214832143d38
Unknown <ahmed.m.rashed@gmail.com>
2020-05-09 14:37:08 +0200

    Upgrade Visual Studio to version 2019 and Matlab 2019b

--- Matlab/MDOF_FRF/MDOF_FRF_Visc.m
+++ Matlab/MDOF_FRF/MDOF_FRF_Visc.m
@@ -1,12 +1,12 @@
 function H_w_n_m_cols=MDOF_FRF_Visc(EigValues_vec, EigVectors_Normalized, w_column, n_row, m_row)
 
 N=size(EigVectors_Normalized,1);
-n_w=size(w_column,1);
+N_w=size(w_column,1);
 if (any(size(n_row)~=size(m_row)));error('Dimensions of n_row and m_row must be identical');end
 N_cols=size(n_row,2);
 
-H_w_n_m_cols_SDOF=zeros(n_w,N_cols);
-H_w_n_m_cols=zeros(n_w,N_cols);
+H_w_n_m_cols_SDOF=zeros(N_w,N_cols);
+H_w_n_m_cols=zeros(N_w,N_cols);
 A_ind_row=sub2ind([N,N],n_row,m_row);
 for r=1:2*N
     A_r=EigVectors_Normalized(:,r)*EigVectors_Normalized(:,r).';
@@ -18,5 +18,5 @@ for r=1:2*N
     end
     
     H_w_n_m_cols=H_w_n_m_cols+H_w_n_m_cols_SDOF;
-    H_w_n_m_cols_SDOF=zeros(n_w,N_cols);
+    H_w_n_m_cols_SDOF=zeros(N_w,N_cols);
 end
