a69e3e88f2190fd4e3c9110867029ed9fb93421a
Ahmed Rashed <ahmed.m.rashed@gmail.com>
2020-08-02 16:23:47 +0200

    Temporary

--- Matlab/shared/export_figure.m
+++ Matlab/shared/export_figure.m
@@ -1,5 +1,5 @@
-function export_figure(fig_handle_vec,  ...
-                    Expand,filenames,resolution,pictureFormat)   %Optional arguments
+function export_figure(fig_handle_vec, ...
+                    Expand,filenames,resolution,pictureFormat_cVec,dimScale)   %Optional arguments
 
 if nargin<2
     Expand='';
@@ -12,23 +12,29 @@ elseif isempty(resolution)
 end
 
 if nargin<5
-    pictureFormat={'pdf'};
+    pictureFormat_cVec={'pdf'};
+elseif isempty(pictureFormat_cVec)
+    pictureFormat_cVec={'pdf'};
 else
-    if ~iscell(pictureFormat)
+    if ~iscell(pictureFormat_cVec)
         error('pictureFormat must be cell array of strings.')
     end
 end
 
-printFlag=cell(size(pictureFormat));
-for n=1:length(pictureFormat)
-    if strcmpi(pictureFormat{n},'emf')
+if nargin<6
+    dimScale=[];
+end
+
+printFlag_cVec=cell(size(pictureFormat_cVec));
+for n=1:length(pictureFormat_cVec)
+    if strcmpi(pictureFormat_cVec{n},'emf')
         if ispc
-            printFlag{n}='meta';
+            printFlag_cVec{n}='meta';
         else
             error('Matlab cannot export emf except under Windows.');
         end
     else
-        printFlag{n}=lower(pictureFormat{n});
+        printFlag_cVec{n}=lower(pictureFormat_cVec{n});
     end
 end
 
@@ -60,16 +66,16 @@ for i=1:length(fig_handle_vec)
     if ~isempty(Expand)
         if ischar(Expand)
             if strcmpi(Expand(1:2),'||')
-                 set(fig_handle_vec(i), 'PaperOrientation', 'portrait');
+                 set(fig_handle_vec(i),'PaperOrientation','portrait');
             elseif strcmpi(Expand(1:2),'==')
-               set(fig_handle_vec(i), 'PaperOrientation', 'landscape');
+               set(fig_handle_vec(i),'PaperOrientation','landscape');
             end
         end
         
         if ischar(Expand)
             if strcmpi(Expand,'||') || strcmpi(Expand,'==')
                 a=get(fig_handle_vec(i),'papersize');
-                set(fig_handle_vec(i), 'PaperPositionMode', 'manual');
+                set(fig_handle_vec(i),'PaperPositionMode','manual');
                 set(fig_handle_vec(i),'PaperPosition',[0 0 a(1) a(2)]);
                 set(fig_handle_vec(i),'Units',get(fig_handle_vec(i),'PaperUnits'));
                 set(fig_handle_vec(i),'Position',[0 0 a(1) a(2)]);
@@ -77,41 +83,62 @@ for i=1:length(fig_handle_vec)
                 set(0,'CurrentFigure',fig_handle_vec(i)),
                 drawnow
             else
-                set(fig_handle_vec(i), 'PaperPositionMode', 'auto');
+                set(fig_handle_vec(i),'PaperPositionMode','auto');
             end
-        elseif isnumeric(Expand)
+        end
+        if ~isempty(dimScale)
             pos=get(fig_handle_vec(i),'PaperPosition');
-            set(fig_handle_vec(i), 'PaperPositionMode', 'manual');
-            set(fig_handle_vec(i),'PaperPosition',[pos(1:2),pos(3:4)*Expand]);
-
+            set(fig_handle_vec(i),'PaperPositionMode','manual');
+            set(fig_handle_vec(i),'PaperPosition',[pos(1:2),pos(3:4).*dimScale/max(dimScale)]);
         end
     end
 end
 
 for i=1:length(fig_handle_vec)
-    for n=1:length(printFlag)
+    for n=1:length(printFlag_cVec)
+        if any(strcmp(printFlag_cVec{n},{'meta','pdf','eps','epsc','svg'}))
+            renderer='-painters';
+        elseif any(strcmp(printFlag_cVec{n},{'png','jpg'}))
+            renderer='-opengl';
+        end
         if nargin<3
-           print(['-r',int2str(resolution)], '-painters', ['-d',printFlag{n}],['-f',int2str(double(fig_handle_vec(i)))]);
-           %print(['-r',int2str(resolution)], '-painters', ['-d',printFlag{n}],['-f',int2str(get(fig_handle_vec(i),'Number'))]);
+           print(['-r',int2str(resolution)],renderer,['-d',printFlag_cVec{n}],['-f',int2str(double(fig_handle_vec(i)))]);
         else
-           print(['-r',int2str(resolution)], '-painters', ['-d',printFlag{n}],['-f',int2str(double(fig_handle_vec(i)))],[filenames{i},['.',pictureFormat{n}]]);
-%           print(['-r',int2str(resolution)], '-painters', ['-d',printFlag{n}],['-f',int2str(get(fig_handle_vec(i),'Number'))],[filenames{i},['.',pictureFormat{n}]]);
+           print(['-r',int2str(resolution)],renderer,['-d',printFlag_cVec{n}],['-f',int2str(double(fig_handle_vec(i)))],[filenames{i},'.',pictureFormat_cVec{n}]);
         end
     end
 end
 
 %If "strawberry perl" and Miketex is installed
-if nargin>=3 %&& ispc
+if nargin>=3
     temp_env=getenv('LD_LIBRARY_PATH');
-    setenv('LD_LIBRARY_PATH', '')
-    for n=1:length(pictureFormat)
-        if strcmpi(pictureFormat{n},'pdf')
-           for i=1:length(fig_handle_vec)
-               system(['pdfcrop "',filenames{i},'.pdf" "',filenames{i},'.pdf"']);
-           end
-
-           break;
+    setenv('LD_LIBRARY_PATH','')
+    
+    if any(strcmpi(pictureFormat_cVec,'pdf'))
+        [status,~]=system('where pdfcrop');
+        if status,warning('pdfcrop is not installed. Please install it through TeXLive or MiKTeX.'),end
+    end
+    
+    if any(strcmpi(pictureFormat_cVec,'png')) || any(strcmpi(pictureFormat_cVec,'jpg'))
+        if ispc
+            [status,~]=system('where magick');
+            if status,warning('Imagemagick is not installed.'),end
+        else
+            [status,~]=system('where convert');
+            if status,warning('Imagemagick is not installed.'),end
         end
     end
-    setenv('LD_LIBRARY_PATH', temp_env)
+
+    for n=1:length(pictureFormat_cVec)
+        if strcmpi(pictureFormat_cVec{n},'pdf')
+            for i=1:length(fig_handle_vec)
+               system(['pdfcrop "',filenames{i},'.pdf" "',filenames{i},'.pdf"']);
+            end
+        elseif any(strcmpi(pictureFormat_cVec{n},{'png','jpg'}))
+            for i=1:length(fig_handle_vec)
+                system(['magick convert "',filenames{i},'.',pictureFormat_cVec{n},'" -trim "',filenames{i},'.',pictureFormat_cVec{n},'"']);
+            end
+        end
+    end
+    setenv('LD_LIBRARY_PATH',temp_env)
 end
