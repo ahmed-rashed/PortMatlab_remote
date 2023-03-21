f8ed9a31b16e219b28d793781e0035d484e15279
Ahmed Rashed <ahmed.m.rashed@gmail.com>
2023-03-21 10:38:22 +0200

    Update

---
+++ Julia/Free_response_main.jl
@@ -0,0 +1,29 @@
+using Plots
+include("Free_Response.jl")
+
+x₀=-1;
+v₀=0;
+ωₙ=1;
+fₙ=ωₙ/2/π;
+Tₙ=1/fₙ;
+
+zeta_vec=[0,.1,.2,.4,1/√2,1,2];
+legend_str = ["\$\\zeta = $i \$" for i in ∪(0:0.1:0.2,[0.4,1/√2,1,2])]
+legend_str[5] = "\$\\zeta = 1/\\sqrt{2}\$"
+t_vec= 0:2*Tₙ/(500-1):2*Tₙ
+
+plot()
+for (n, ζ) in enumerate(zeta_vec)
+    x_vec=Free_Response(ωₙ,ζ,x₀,v₀,t_vec);
+    plot!(t_vec/Tₙ,x_vec,label = legend_str[n])
+end
+p = plot!(legend=:bottomright,grid = true)
+
+title!("\$x(t)\$ for \$\\omega_{n}=1\$, \$x_{0}=-1\$ and \$v_{0}=0\$")
+xlabel!("\$t/T_{n}\\qquad,:T_{n}=1/f_{n}=2\\pi/\\omega_{n}\$")
+
+display(p)
+
+png(p,"SDOF_1")
+Plots.pdf(p, "SDOF_1")
+Plots.svg(p, "SDOF_1")
